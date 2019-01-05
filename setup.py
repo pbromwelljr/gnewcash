222427a125827d8247ca60e769e401d13a8daf7c
Paul Bromwell Jr <pbromwelljr@gmail.com>
2019-01-04 23:16:16 -0500

    Final changes before pushing version 1.0.0 to PyPI.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(file_name):
@@ -8,18 +8,26 @@ def read(file_name):
 
 setup(
     name='gnewcash',
-    version='0.1',
+    version='1.0.0',
     description='Python Library for reading, interacting with, and writing GnuCash files',
     author='Paul Bromwell Jr.',
-    author_email='pbromwelljr@outlook.com',
-    packages=[],
+    author_email='pbromwelljr@gmail.com',
+    packages=find_packages(),
     license='MIT',
     keywords='gnucash finance finances cash personal banking',
-    url='http://packages.python.org/gnewcash',
+    url='https://github.com/pbromwelljr/gnewcash',
     long_description=read('README.md'),
+    long_description_content_type='text/markdown',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Topic :: Finances',
+        'Development Status :: 4 - Beta',
+        'Topic :: Office/Business :: Financial :: Accounting',
         'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
